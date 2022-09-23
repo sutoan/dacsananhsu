4f0253124855cd5461434ce62fddcdf47a89cf85
sutoan <93644983+sutoan@users.noreply.github.com>
2022-09-23 14:29:41 +0700

    admin update 3

--- admin/js/todolist.js
+++ admin/js/todolist.js
@@ -9,7 +9,13 @@
       var item = $(this).prevAll('.todo-list-input').val();
 
       if (item) {
-        todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>" + item + "<i class='input-helper'></i></label></div><i class='remove ti-close'></i></li>");
+        todoListItem.append(`
+        <li>
+          <div class='form-check'>
+            <label class='form-check-label'>
+              <input class='checkbox' type='checkbox'/>" + item + "<i class='input-helper'></i>
+            </label></div><i class='remove ti-close'></i>
+        </li>`);
         todoListInput.val("");
       }
 
